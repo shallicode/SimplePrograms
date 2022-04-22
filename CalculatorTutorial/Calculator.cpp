7dcb08305c75ba30cff951f1db08fa2cb117686b
shall <shall@LAPTOP-E9N58LDL>
2022-04-22 12:20:08 +0530

    Add project files.

---
+++ CalculatorTutorial/Calculator.cpp
@@ -0,0 +1,20 @@
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    case '%':
+        return (int)x % (int)y;
+    default:
+        return 0.0;
+    }
+}
